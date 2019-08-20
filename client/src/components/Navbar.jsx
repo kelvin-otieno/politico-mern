0fcd1afd45a12befc02293ecafc36da12e4a5ce9
kelvin-otieno <kelvinotieno06@gmail.com>
2019-08-20 23:30:47 +0300

    Commit with all api done and starting client side

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,125 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <a className="navbar-brand" href="index.html">Politico</a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active" >
+                        <a className="nav-link" href="index.html"
+                        >Home </a
+                        >
+                    </li>
+
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Party
+          </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="parties.html">Parties</a>
+                            <a className="dropdown-item" href="create_party.html">Create Party</a>
+                        </div>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Office
+          </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="offices.html">Offices</a>
+                            <a className="dropdown-item" href="create_office.html">Create Office</a>
+                        </div>
+                    </li>
+
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Candidate
+        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="candidates.html">Candidates</a>
+                            <a className="dropdown-item" href="create_candidate.html">Create Candidate</a>
+                        </div>
+                    </li>
+                    <li className="nav-item" >
+                        <a className="nav-link" href="vote.html"
+                        >Vote</a
+                        >
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Petition
+        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="petitions.html">Petitions</a>
+                            <a className="dropdown-item" href="create_petition.html">Create Petition</a>
+                        </div>
+                    </li>
+
+
+                </ul>
+                <ul className="nav navbar-nav ml-auto mr-4">
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            <img src="images/user.png" className="rounded-circle user-logo" alt="" />
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="login.html">Login</a>
+                            <a className="dropdown-item" href="register.html">Register</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
